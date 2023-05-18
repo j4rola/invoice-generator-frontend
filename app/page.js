9070acdab78342a33d504d286ed68cd41a5e451a
Joe Froula <jmfroula@gmail.com>
2023-05-17 20:50:40 -0700

    fixes import

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Header from '@/components/Header'
 import Test from '@/components/Test'
-import InvoiceForm from '@/components/invoiceForm'
+import InvoiceForm from '@/components/InvoiceForm'
 
 export default function Home() {
   return (
