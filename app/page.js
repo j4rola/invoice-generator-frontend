c58c0c8ac9b17a7ec16fddb8179528ea96369b48
Joe Froula <jmfroula@gmail.com>
2023-05-24 12:16:49 -0700

    test

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from '@/components/Header'
-import Test from '@/components/Test'
 import InvoiceForm from '@/components/InvoiceForm'
 
 
 export default function Home() {
   return (
     <div>
-      <Test></Test>
       <InvoiceForm></InvoiceForm>
     </div>
   )
