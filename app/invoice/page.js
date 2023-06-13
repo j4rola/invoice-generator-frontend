355abceb30b08d7dde2c4d5ee0374d046a5ba98d
Joe Froula <jmfroula@gmail.com>
2023-06-13 12:15:21 -0700

    test

--- app/invoice/page.js
+++ app/invoice/page.js
@@ -11,16 +11,16 @@ export default function Home() {
   return (
     <>
     <div className='flex'>
-    <div className='w-100 red' ></div>
-    <h1 id='title' style={{color: 'red'}} className='flex'></h1>
+    <div className='w-100 red' ></div> 
+    <h1 id='title' style={{color: 'red', marginLeft: '100px'}} className='flex'></h1>
     
-    <div id='paymentTerms'>
+    <div id='paymentTerms'>  
             
     </div>
     <div id='invoiceDate'>  
           
   </div>
-  <div id='invoiceAmount'>
+  <div id='invoiceAmount'>  
       
   </div>
   </div>
